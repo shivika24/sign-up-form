0483883434db7767054b547d852467b730bfc744
shivika24 <43493690+shivika24@users.noreply.github.com>
2020-10-01 19:32:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -161,3 +161,18 @@
           cell6.innerHTML = `${item.password}`;
       });
      }
+document.getElementById("my_captcha_form").addEventListener("submit",function(evt)
+  {
+  
+  var response = grecaptcha.getResponse();
+  if(response.length == 0) 
+  { 
+    //reCaptcha not verified
+    alert("please verify you are humann!"); 
+    evt.preventDefault();
+    return false;
+  }
+  //captcha verified
+  //do the rest of your validations here
+  
+});
